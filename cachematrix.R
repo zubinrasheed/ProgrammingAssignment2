3746dda83a9ef3647ed8b81bb0d9048325dc8cee
Zubin Rasheed <zubinrasheed@gmail.com>
2014-09-21 14:06:33 -0500

    Updated Prog Assignment2 - Zubin Rasheed

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,44 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+# Initialize Cache to Null
+matCache <- NULL
+
+
+# Set New Matrix Set
+SetMatrix <- function(y)
+
+   {
+       x <<- y
+       matCache <- NULL
+
+   }
+
+
+
+# Get the Matrix
+GetMatrix <- function(){ x
+}
+
+#Cache Matrix Inverse
+CacheInverse <- function(matinv)
+
+    {
+       matCache <<- matinv
+
+    }
+
+
+#Get Matrix Inverse
+getInverse <- function () 
+
+  {
+     matCache
+
+  }
+
+#Create the Function Vector
+list(SetMatrix=SetMatrix, GetMatrix=GetMatrix, CacheInverse=CacheInverse, getInverse=getInverse)
 
 }
 
@@ -12,4 +50,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		if (is.null(x$getInverse())) {
+                                res <- solve(x$GetMatrix()) 
+                                print("Calculating Result") 
+                                x$CacheInverse(res)
+                                return (res)
+                            }
+
+else { 
+             print("Getting Result from Cache")
+             return (x$getInverse())
+
+      }
+
+
+
 }
